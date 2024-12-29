d7df3ad32fdebd5b151c2bf32b6fe539cabc6f75
islahdev <islahdev@gmail.com>
2024-12-29 22:51:34 +0700

    upade 3

--- apple.php
+++ apple.php
@@ -1,6 +1,10 @@
 <?php
 
 
-$output = shell_exec('cd /public_html/goapi.qordinate.com/tes_serv_shared && git pull origin main 2>&1');
-echo "<pre>$output</pre>";
+// $output = shell_exec('cd /public_html/goapi.qordinate.com/tes_serv_shared && git pull origin main 2>&1');
+// echo "<pre>$output</pre>";
+echo shell_exec('whoami');
+
+// echo shell_exec('which git');
+echo "apple1";
 
