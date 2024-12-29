f49ad01925f26d5ee22169431debf5741ec338f2
islahdev <islahdev@gmail.com>
2024-12-29 23:24:34 +0700

    upade 11

--- buku.php
+++ buku.php
@@ -1,3 +1,21 @@
 <?php
+require_once 'vendor/autoload.php';
 
-echo "buku";
+use Cz\Git\GitRepository;
+
+// Path ke folder proyek
+$repoPath = '/public_html/goapi.qordinate.com/tes_serv_shared';
+
+try {
+    // Buka repository
+    $repo = new GitRepository($repoPath);
+
+    // Tarik pembaruan dari branch utama
+    $repo->pull('origin', 'main');
+
+    echo "Deployment successful.";
+} catch (Exception $e) {
+    // file_put_contents('webhook.log', $e->getMessage(), FILE_APPEND);
+    exit('Deployment failed: ' . $e->getMessage());
+}
+?>
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e71ff8df9486b15f521d0e46eee82f71d6ae382b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e71ff8df9486b15f521d0e46eee82f71d6ae382b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'czproject/git-php' => array(
+            'pretty_version' => 'v4.3.0',
+            'version' => '4.3.0.0',
+            'reference' => 'f7fe85cc526c5f8e7ed809f7f9c68f2696940a8a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../czproject/git-php',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
